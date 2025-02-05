9627b6d56903e3da7f70bc059f11e99230dd67fa
lucifer-prashant <22051530@kiit.ac.in>
2025-02-05 21:03:12 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import {
 	Braces,
 } from "lucide-react"
 
+import { Analytics } from "@vercel/analytics/react"
 import ContactForm from "./components/ContactForm"
 import { motion } from "framer-motion"
 import { useSectionNavigation } from "./components/useSectionNavigation"
